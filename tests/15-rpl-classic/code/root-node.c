fbab7e1932522f4da0a7419ecdd02c0dda6ff1f0
weiyiiii <117815704+weiyi0411@users.noreply.github.com>
2024-03-12 16:22:28 +0100

    Update root-node.c
    test energest phase

--- tests/15-rpl-classic/code/root-node.c
+++ tests/15-rpl-classic/code/root-node.c
@@ -1,3 +1,95 @@
+// /*
+//  * Copyright (c) 2012, Thingsquare, www.thingsquare.com.
+//  * All rights reserved.
+//  *
+//  * Redistribution and use in source and binary forms, with or without
+//  * modification, are permitted provided that the following conditions
+//  * are met:
+//  * 1. Redistributions of source code must retain the above copyright
+//  *    notice, this list of conditions and the following disclaimer.
+//  * 2. Redistributions in binary form must reproduce the above copyright
+//  *    notice, this list of conditions and the following disclaimer in the
+//  *    documentation and/or other materials provided with the distribution.
+//  * 3. Neither the name of the Institute nor the names of its contributors
+//  *    may be used to endorse or promote products derived from this software
+//  *    without specific prior written permission.
+//  *
+//  * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+//  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//  * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+//  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+//  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+//  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+//  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+//  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+//  * SUCH DAMAGE.
+//  */
+
+
+// #include "contiki.h"
+// #include "lib/random.h"
+// #include "sys/ctimer.h"
+// #include "sys/etimer.h"
+// #include "net/ipv6/uip.h"
+// #include "net/ipv6/uip-ds6.h"
+// #include "net/ipv6/uip-debug.h"
+// #include "net/routing/routing.h"
+// #include "simple-udp.h"
+
+// #include <stdio.h>
+// #include <string.h>
+
+// #define UDP_PORT 1234
+// #define SERVICE_ID 190
+
+// #define SEND_INTERVAL		(10 * CLOCK_SECOND)
+// #define SEND_TIME		(random_rand() % (SEND_INTERVAL))
+
+// static struct simple_udp_connection unicast_connection;
+
+// /*---------------------------------------------------------------------------*/
+// PROCESS(unicast_receiver_process, "Unicast receiver example process");
+// AUTOSTART_PROCESSES(&unicast_receiver_process);
+// /*---------------------------------------------------------------------------*/
+// static void
+// receiver(struct simple_udp_connection *c,
+//          const uip_ipaddr_t *sender_addr,
+//          uint16_t sender_port,
+//          const uip_ipaddr_t *receiver_addr,
+//          uint16_t receiver_port,
+//          const uint8_t *data,
+//          uint16_t datalen)
+// {
+//   printf("Data received from ");
+//   printf("; ");
+//   uip_debug_ipaddr_print(sender_addr);
+//   // printf(" on port %d from port %d with length %d: '%s'\n",
+//   //        receiver_port, sender_port, datalen, data);
+//   printf("; ");
+//   printf(" %s\n",data);
+// }
+// /*---------------------------------------------------------------------------*/
+// PROCESS_THREAD(unicast_receiver_process, ev, data)
+// {
+//   PROCESS_BEGIN();
+
+//   NETSTACK_ROUTING.root_start();
+
+//   simple_udp_register(&unicast_connection, UDP_PORT,
+//                       NULL, UDP_PORT, receiver);
+
+//   while(1) {
+//     PROCESS_WAIT_EVENT();
+//   }
+//   PROCESS_END();
+// }
+// /*---------------------------------------------------------------------------*/
+
+
+
+
 /*
  * Copyright (c) 2012, Thingsquare, www.thingsquare.com.
  * All rights reserved.
@@ -27,31 +119,30 @@
  * SUCH DAMAGE.
  */
 
-
 #include "contiki.h"
 #include "lib/random.h"
 #include "sys/ctimer.h"
 #include "sys/etimer.h"
 #include "net/ipv6/uip.h"
 #include "net/ipv6/uip-ds6.h"
+#include "net/ipv6/uip-ds6-route.h"
 #include "net/ipv6/uip-debug.h"
-#include "net/routing/routing.h"
+
 #include "simple-udp.h"
+
+#include "net/routing/routing.h"
+#include "dev/leds.h"
 
 #include <stdio.h>
 #include <string.h>
 
 #define UDP_PORT 1234
-#define SERVICE_ID 190
-
-#define SEND_INTERVAL		(10 * CLOCK_SECOND)
-#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
 
 static struct simple_udp_connection unicast_connection;
 
 /*---------------------------------------------------------------------------*/
-PROCESS(unicast_receiver_process, "Unicast receiver example process");
-AUTOSTART_PROCESSES(&unicast_receiver_process);
+PROCESS(receiver_node_process, "Receiver node");
+AUTOSTART_PROCESSES(&receiver_node_process);
 /*---------------------------------------------------------------------------*/
 static void
 receiver(struct simple_udp_connection *c,
@@ -63,26 +154,81 @@ receiver(struct simple_udp_connection *c,
          uint16_t datalen)
 {
   printf("Data received from ");
-  printf("; ");
   uip_debug_ipaddr_print(sender_addr);
-  // printf(" on port %d from port %d with length %d: '%s'\n",
-  //        receiver_port, sender_port, datalen, data);
-  printf("; ");
-  printf(" %s\n",data);
+  printf(" on port %d from port %d with length %d: '%s'\n",
+         receiver_port, sender_port, datalen, data);
 }
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(unicast_receiver_process, ev, data)
+static uip_ipaddr_t *
+set_global_address(void)
 {
+  static uip_ipaddr_t ipaddr;
+  int i;
+  uint8_t state;
+  const uip_ipaddr_t *default_prefix = uip_ds6_default_prefix();
+
+  uip_ip6addr_copy(&ipaddr, default_prefix);
+  uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
+  uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
+
+  printf("IPv6 addresses: ");
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+    state = uip_ds6_if.addr_list[i].state;
+    if(uip_ds6_if.addr_list[i].isused &&
+       (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
+      uip_debug_ipaddr_print(&uip_ds6_if.addr_list[i].ipaddr);
+      printf("\n");
+    }
+  }
+
+  return &ipaddr;
+}
+/*---------------------------------------------------------------------------*/
+#if RPL_WITH_STORING
+uint8_t should_blink = 1;
+static void
+route_callback(int event, const uip_ipaddr_t *route, const uip_ipaddr_t *ipaddr, int num_routes)
+{
+  if(event == UIP_DS6_NOTIFICATION_DEFRT_ADD) {
+    should_blink = 0;
+  } else if(event == UIP_DS6_NOTIFICATION_DEFRT_RM) {
+    should_blink = 1;
+  }
+}
+#endif /* #if RPL_WITH_STORING */
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(receiver_node_process, ev, data)
+{
+  static struct etimer et;
+#if RPL_WITH_STORING
+  static struct uip_ds6_notification n;
+#endif /* #if RPL_WITH_STORING */
+
   PROCESS_BEGIN();
 
-  NETSTACK_ROUTING.root_start();
+  set_global_address();
+
+#if RPL_WITH_STORING
+  uip_ds6_notification_add(&n, route_callback);
+#endif /* #if RPL_WITH_STORING */
 
   simple_udp_register(&unicast_connection, UDP_PORT,
                       NULL, UDP_PORT, receiver);
 
+  etimer_set(&et, CLOCK_SECOND);
   while(1) {
-    PROCESS_WAIT_EVENT();
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+    etimer_reset(&et);
+#if RPL_WITH_STORING
+    if(should_blink) {
+      leds_on(LEDS_ALL);
+      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+      etimer_reset(&et);
+      leds_off(LEDS_ALL);
+    }
+#endif /* #if RPL_WITH_STORING */
   }
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
+
